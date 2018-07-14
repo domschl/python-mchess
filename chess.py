bcfa0cc24a6ee888ce5657a8c5a4250db5980518
Dominik Schloesser <dsc@dosc.net>
2018-07-14 11:35:13 +0200

    test version 1

--- chess.py
+++ chess.py
@@ -1,34 +1,64 @@
 import serial
+import time
 
 
 class MillenniumChess:
     def __init__(self, port):
         self.ser_port = serial.Serial(port, 38400, timeout=1)
+        self.ser_port.dtr = 0
 
-    def add_odd_par(b):
-        byte = b & 127
+    def add_odd_par(self, b):
+        byte = ord(b) & 127
         par = 1
-        for i in range(7):
-            bit = byte >> 1
-            byte = byte / 2
+        for _ in range(7):
+            bit = byte & 1
+            byte = byte >> 1
             par = par ^ bit
         if par == 1:
-            byte = b | 128
+            byte = ord(b) | 128
         else:
-            byte = b & 127
+            byte = ord(b) & 127
         return byte
 
-    def write(bytes):
+    def hexd(self, digit):
+        if digit < 10:
+            return chr(ord('0')+digit)
+        else:
+            return chr(ord('A')-10+digit)
+
+    def hex(self, num):
+        d1 = num//16
+        d2 = num % 16
+        s = self.hexd(d1)+self.hexd(d2)
+        print(" cnv<{} {}>".format(num, s))
+        return s
+
+    def write(self, bytes):
         gpar = 0
         for b in bytes:
-            gpar = gpar ^ b
+            gpar = gpar ^ ord(b)
+        bytes = bytes+self.hex(gpar)
+        for b in bytes:
+            bo = self.add_odd_par(b)
+            print("[>{} {}]".format(b, bo))
+            self.ser_port.write(bo)
+
+    def read(self, num):
+        for _ in range(num):
+            b = self.ser_port.read(1)
+            print("[{} {}]".format(b, str(b)))
 
     def disconnect(self):
         self.ser_port.close()
 
 
 if __name__ == '__main__':
-    print("Staring!")
+    print("Starting!")
     port = '/dev/tty.Bluetooth-Incoming-Port'
     board = MillenniumChess(port)
+    board.write("V")
+
+    time.sleep(0.1)
+    board.read(7)
+    board.disconnect()
     print("closed.")
