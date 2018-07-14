737496b83988073d15f3ee2984560315b75679c1
Dominik Schloesser <dsc@dosc.net>
2018-07-14 13:06:06 +0200

    Working USB

--- chess.py
+++ chess.py
@@ -4,8 +4,8 @@ import time
 
 class MillenniumChess:
     def __init__(self, port):
-        self.ser_port = serial.Serial(port, 38400, timeout=1)
-        self.ser_port.dtr = 1
+        self.ser_port = serial.Serial(port, 38400)  # , timeout=1)
+        self.ser_port.dtr = 0
 
     def add_odd_par(self, b):
         byte = ord(b) & 127
@@ -15,8 +15,8 @@ class MillenniumChess:
             byte = byte >> 1
             par = par ^ bit
         if par == 1:
-            byte = ord(b) & 127
-            # byte = ord(b) | 128
+            # byte = ord(b) & 127
+            byte = ord(b) | 128
         else:
             byte = ord(b) & 127
         return byte
@@ -34,21 +34,29 @@ class MillenniumChess:
         print(" cnv<{} {}>".format(num, s))
         return s
 
-    def write(self, bytes):
+    def write(self, msg):
         gpar = 0
-        for b in bytes:
+        for b in msg:
             gpar = gpar ^ ord(b)
-        bytes = bytes+self.hex(gpar)
-        print("-> {}".format(bytes))
-        for b in bytes:
-            bo = self.add_odd_par(b)
-            print("[>{} {}]".format(b, bo))
-            self.ser_port.write(bo)
+        msg = msg+self.hex(gpar)
+        print("-> {}".format(msg))
+        bts = []
+        for c in msg:
+            bo = self.add_odd_par(c)
+            bts.append(bo)
+        n = self.ser_port.write(bts)
+        self.ser_port.flush()
+        print()
+        print("Written: {}".format(n))
 
     def read(self, num):
         for _ in range(num):
-            b = self.ser_port.read(1)
-            print("[{} {}]".format(b, str(b)))
+
+            try:
+                b = self.ser_port.read()
+                print("[{}]".format(b))
+            except:
+                pass
 
     def disconnect(self):
         self.ser_port.close()
@@ -56,11 +64,29 @@ class MillenniumChess:
 
 if __name__ == '__main__':
     print("Starting!")
-    port = '/dev/tty.MILLENNIUMCHESS-SerialP'
+    # port = '/dev/tty.MILLENNIUMCHESS-SerialP'
+    port = '/dev/ttyUSB0'  # rfcomm1'
     board = MillenniumChess(port)
-    board.write("V")
 
+    cmd = "L50"
+    for _ in range(81):
+        cmd = cmd + "C4"
+    board.write(cmd)
+    # board.write("V")
     # time.sleep(0.1)
-    board.read(7)
+    board.read(3)
+    # board.write("S")
+    # board.read(10)
+
+    time.sleep(5)
+    cmd = "X"
+    board.write(cmd)
+    board.read(3)
+
+    time.sleep(1)
+    cmd = "V"
+    board.write(cmd)
+    board.read(3)
+
     board.disconnect()
     print("closed.")
