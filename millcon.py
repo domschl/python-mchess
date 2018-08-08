2c195a1aa0585d79c4f6293730eb5a92bf40abf2
Dominik Schlösser <dsc@dosc.net>
2018-08-08 13:01:59 +0200

    ongoing

--- millcon.py
+++ millcon.py
@@ -124,11 +124,123 @@ class MillenniumChess:
         while self.thread_active:
             if self.trque.empty() is False:
                 msg = self.trque.get()
-                self.log.debug(
-                    'Millenium received {}'.format(msg))
-                self.appque.put(msg)
+                if len(msg) > 0:
+                    if msg[0] == 's':
+                        if len(msg) == 67:
+                            rp = msg[1:65]
+                            val_pos = True
+                            position = [
+                                [0 for x in range(8)] for y in range(8)]
+                            if len(rp) == 64:
+                                for y in range(8):
+                                    for x in range(8):
+                                        c = rp[7-x+y*8]
+                                        i = self.figrep['ascii'].find(c)
+                                        if i == -1:
+                                            self.log.warning(
+                                                "Invalid char in raw position: {}".format(c))
+                                            val_pos = False
+                                            continue
+                                        else:
+                                            f = self.figrep['int'][i]
+                                            position[y][x] = f
+                            else:
+                                val_pos = False
+                                self.log.warning(
+                                    "Error in board position, received {}".format(len(rp)))
+                                continue
+                        else:
+                            val_pos = False
+                            self.log.error(
+                                'Incomplete board position, {}'.format(msg))
+                        if val_pos is True:
+                            self.position = position
+                            self.print_position_ascii(position)
+                            fen = self.position_to_fen(position)
+                            self.appque.put({'fen': fen})
             else:
                 time.sleep(0.1)
+
+    def position_to_fen(self, position):
+        fen = ""
+        blanks = 0
+        for y in range(8):
+            for x in range(8):
+                f = position[7-y][x]
+                c = '?'
+                for i in range(len(self.figrep['int'])):
+                    if self.figrep['int'][i] == f:
+                        c = self.figrep['ascii'][i]
+                        break
+                if c == '?':
+                    self.log.error(
+                        "Internal FEN error, could not translation {} at {}{}".format(c, y, x))
+                    return ""
+                if c == '.':
+                    blanks = blanks + 1
+                else:
+                    if blanks > 0:
+                        fen += str(blanks)
+                        blanks = 0
+                    fen += c
+            if blanks > 0:
+                fen += str(blanks)
+                blanks = 0
+            if y < 7:
+                fen += '/'
+        fen += ' w KQkq - 0 1'
+        return fen
+
+    def fen_to_position(self, fen):
+        position = [[0 for x in range(8)] for y in range(8)]
+        fenp = fen[:fen.find(' ')]
+        fi = 0
+        for y in range(8):
+            x = 0
+            while x < 8:
+                c = fenp[fi]
+                fi += 1
+                if c >= '1' and c <= '8':
+                    x += int(c)
+                    continue
+                ci = -99
+                for i in range(len(self.figrep['ascii'])):
+                    if self.figrep['ascii'][i] == c:
+                        ci = self.figrep['int'][i]
+                        break
+                if ci == -99:
+                    self.log.error(
+                        "Internal FEN2 error decoding {} at {}{}".format(c, y, x))
+                    return []
+                position[7-y][x] = ci
+                x += 1
+            if y < 7 and fenp[fi] != '/':
+                self.log.error(
+                    "Illegal fen: missing '/' {}{}: {}[{}]".format(y, x, fenp[fi], fi))
+                return []
+            fi += 1
+        return position
+
+    def print_position_ascii(self, position):
+        print("  +------------------------+")
+        for y in range(8):
+            print("{} |".format(8-y), end="")
+            for x in range(8):
+                f = position[7-y][x]
+                if (x+y) % 2 == 0:
+                    f = f*-1
+                c = '?'
+                for i in range(len(self.figrep['int'])):
+                    if self.figrep['int'][i] == f:
+                        c = self.figrep['unic'][i]
+                        break
+                if (x+y) % 2 == 0:
+                    print("\033[7m {} \033[m".format(c), end="")
+                else:
+                    print(" {} ".format(c), end='')
+            print("|")
+        print("  +------------------------+")
+        print("    A  B  C  D  E  F  G  H")
 
     def _open_transport(self, transport):
         try:
@@ -166,6 +278,15 @@ class MillenniumChess:
 
 
 if __name__ == '__main__':
+
+    if platform.system().lower() == 'windows':
+        from ctypes import windll, c_int, byref
+        stdout_handle = windll.kernel32.GetStdHandle(c_int(-11))
+        mode = c_int(0)
+        windll.kernel32.GetConsoleMode(c_int(stdout_handle), byref(mode))
+        mode = c_int(mode.value | 4)
+        windll.kernel32.SetConsoleMode(c_int(stdout_handle), mode)
+
     logging.basicConfig(
         format='%(asctime)s %(levelname)s %(name)s %(message)s', level=logging.DEBUG)
     appque = queue.Queue()
