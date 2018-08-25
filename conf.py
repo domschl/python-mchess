81269d3f9fa0554d870e65342b6f8f3070848ebd
Dominik Schloesser <dsc@dosc.net>
2018-08-25 20:13:47 +0200

    more tests

--- conf.py
+++ conf.py
@@ -41,7 +41,7 @@ release = '0.1.0'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    #    'sphinx.ext.githubpages',
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
