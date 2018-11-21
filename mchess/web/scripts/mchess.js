e831a56b372475511b213d747b920ea684ab0392
Dominik Schloesser <dsc@dosc.net>
2018-11-21 13:47:19 +0100

    websocket auto reconnect and UI-Status

--- mchess/web/scripts/mchess.js
+++ mchess/web/scripts/mchess.js
@@ -4,7 +4,7 @@ import {
     Chessboard
 } from "../node_modules/cm-chessboard/src/cm-chessboard/Chessboard.js"
 
-var mchessSocket = new WebSocket("ws://" + window.location.host + "/ws");
+
 var mainBoard = null;
 var miniBoard1 = null;
 var miniBoard2 = null;
@@ -12,167 +12,180 @@ var VariantInfo = null;
 var FenRef = {};
 var StatHeader = {};
 
-mchessSocket.onopen = function (event) { }
+wsConnect("ws://" + window.location.host + "/ws");
 
-mchessSocket.onmessage = function (event) {
-    var msg;
-    try {
-        msg = JSON.parse(event.data);
-    } catch (err) {
-        console.log('JSON error: ' + err.message);
-        return;
+function wsConnect(address) {
+    var mchessSocket = new WebSocket(address);
+    mchessSocket.onopen = function (event) {
+        document.getElementById("connect-state").style.color = "#58A4B0";
+        document.getElementById("connect-text").innerText = "connected";
     }
-    // console.log("got message: ")
-    // console.log(msg)
-    if (msg.hasOwnProperty("fen") && msg.hasOwnProperty("attribs") && msg.hasOwnProperty("pgn")) {
-        console.log("got board position.");
-        console.log(msg.pgn)
-        if (VariantInfo != null) {
-            for (var a in VariantInfo) {
-                VariantInfo[a] = {}
-                FenRef[a] = msg.fen;
-            }
+    mchessSocket.onclose = function () {
+        // Try to reconnect in 1 seconds
+        document.getElementById("connect-state").style.color = "red";
+        document.getElementById("connect-text").innerText = "disconnected";
+        mchessSocket = null;
+        setTimeout(function () { wsConnect(address) }, 1000);
+    };
+    mchessSocket.onmessage = function (event) {
+        var msg;
+        try {
+            msg = JSON.parse(event.data);
+        } catch (err) {
+            console.log('JSON error: ' + err.message);
+            return;
         }
-        StatHeader = {};
-        document.getElementById("miniinfo1").innerHTML = "";
-        document.getElementById("miniinfo2").innerHTML = "";
-        document.getElementById("ph21").innerHTML = "";
-        document.getElementById("ph31").innerHTML = "";
-        var title = msg.attribs.white_name + " - " + msg.attribs.black_name;
-        console.log(msg.fen)
-        if (mainBoard == null) {
-            mainBoard = new Chessboard(document.getElementById("board1"), {
-                position: msg.fen,
-                style: {
-                    showCoordinates: true,
-                    showBorder: true,
-                },
-                responsive: true,
-                sprite: {
-                    url: "node_modules/cm-chessboard/assets/images/chessboard-sprite.svg"
+        // console.log("got message: ")
+        // console.log(msg)
+        if (msg.hasOwnProperty("fen") && msg.hasOwnProperty("attribs") && msg.hasOwnProperty("pgn")) {
+            console.log("got board position.");
+            console.log(msg.pgn)
+            if (VariantInfo != null) {
+                for (var a in VariantInfo) {
+                    VariantInfo[a] = {}
+                    FenRef[a] = msg.fen;
                 }
-            });
-            var brd = document.getElementsByClassName("board");
-            document.getElementById("board1").style.height = "260px";
-            document.getElementById("board1").style.width = "260px";
-            console.log(brd[0].style.width);
-            //document.getElementById("ph1").style.width = brd[0].style.width;
-        } else {
-            mainBoard.setPosition(msg.fen);
-        }
-        document.getElementById("ph1").innerText = title;
-        var pi = msg.pgn.search("\n\n");
-        var pgn = msg.pgn;
-        if (pi != -1) {
-            pgn = msg.pgn.substring(pi);
-        }
-        pgn = pgn.replace(" *", "");
-        pgn = pgn.replace(" ", "&nbsp;")
-        var regex = /([0-9]+\.)/g;
-        pgn = pgn.replace(regex, "<span class=\"movenrb\"> &nbsp;$1</span>")
-        document.getElementById("mainmoves").innerHTML = pgn;
+            }
+            StatHeader = {};
+            document.getElementById("miniinfo1").innerHTML = "";
+            document.getElementById("miniinfo2").innerHTML = "";
+            document.getElementById("ph21").innerHTML = "";
+            document.getElementById("ph31").innerHTML = "";
+            var title = msg.attribs.white_name + " - " + msg.attribs.black_name;
+            console.log(msg.fen)
+            if (mainBoard == null) {
+                mainBoard = new Chessboard(document.getElementById("board1"), {
+                    position: msg.fen,
+                    style: {
+                        showCoordinates: true,
+                        showBorder: true,
+                    },
+                    responsive: true,
+                    sprite: {
+                        url: "node_modules/cm-chessboard/assets/images/chessboard-sprite.svg"
+                    }
+                });
+                var brd = document.getElementsByClassName("board");
+                document.getElementById("board1").style.height = "260px";
+                document.getElementById("board1").style.width = "260px";
+                console.log(brd[0].style.width);
+                //document.getElementById("ph1").style.width = brd[0].style.width;
+            } else {
+                mainBoard.setPosition(msg.fen);
+            }
+            document.getElementById("ph1").innerText = title;
+            var pi = msg.pgn.search("\n\n");
+            var pgn = msg.pgn;
+            if (pi != -1) {
+                pgn = msg.pgn.substring(pi);
+            }
+            pgn = pgn.replace(" *", "");
+            pgn = pgn.replace(" ", "&nbsp;")
+            var regex = /([0-9]+\.)/g;
+            pgn = pgn.replace(regex, "<span class=\"movenrb\"> &nbsp;$1</span>")
+            document.getElementById("mainmoves").innerHTML = pgn;
 
-        if (miniBoard1 == null) {
-            miniBoard1 = new Chessboard(document.getElementById("miniboard1"), {
-                position: msg.fen,
-                style: {
-                    showCoordinates: true,
-                    showBorder: true,
-                },
-                responsive: true,
-                sprite: {
-                    url: "node_modules/cm-chessboard/assets/images/chessboard-sprite.svg"
+            if (miniBoard1 == null) {
+                miniBoard1 = new Chessboard(document.getElementById("miniboard1"), {
+                    position: msg.fen,
+                    style: {
+                        showCoordinates: true,
+                        showBorder: true,
+                    },
+                    responsive: true,
+                    sprite: {
+                        url: "node_modules/cm-chessboard/assets/images/chessboard-sprite.svg"
+                    }
+                });
+                document.getElementById("miniboard1").style.height = "120px";
+                document.getElementById("miniboard1").style.width = "120px";
+            } else {
+                miniBoard1.setPosition(msg.fen);
+            }
+            if (miniBoard2 == null) {
+                miniBoard2 = new Chessboard(document.getElementById("miniboard2"), {
+                    position: msg.fen,
+                    style: {
+                        showCoordinates: true,
+                        showBorder: true,
+                    },
+                    responsive: true,
+                    sprite: {
+                        url: "node_modules/cm-chessboard/assets/images/chessboard-sprite.svg"
+                    }
+                });
+                document.getElementById("miniboard2").style.height = "120px";
+                document.getElementById("miniboard2").style.width = "120px";
+            } else {
+                miniBoard2.setPosition(msg.fen);
+            }
+        } else if (msg.hasOwnProperty("info")) {
+            if (VariantInfo == null) {
+                VariantInfo = {};
+            }
+            if (msg.info.hasOwnProperty("variant")) {
+                console.log("V");
+                var actor = msg.info.actor;
+                var id = msg.info.multipv_ind;
+                if (!(actor in VariantInfo)) {
+                    VariantInfo[actor] = {}
                 }
-            });
-            document.getElementById("miniboard1").style.height = "120px";
-            document.getElementById("miniboard1").style.width = "120px";
-        } else {
-            miniBoard1.setPosition(msg.fen);
-        }
-        if (miniBoard2 == null) {
-            miniBoard2 = new Chessboard(document.getElementById("miniboard2"), {
-                position: msg.fen,
-                style: {
-                    showCoordinates: true,
-                    showBorder: true,
-                },
-                responsive: true,
-                sprite: {
-                    url: "node_modules/cm-chessboard/assets/images/chessboard-sprite.svg"
+                if (id == 1) FenRef[actor] = msg["fenref"];
+                var hd = ""
+                if ("nps" in msg.info) {
+                    hd += " | Nps: " + msg.info.nps;
                 }
-            });
-            document.getElementById("miniboard2").style.height = "120px";
-            document.getElementById("miniboard2").style.width = "120px";
-        } else {
-            miniBoard2.setPosition(msg.fen);
-        }
-    } else if (msg.hasOwnProperty("info")) {
-        if (VariantInfo == null) {
-            VariantInfo = {};
-        }
-        if (msg.info.hasOwnProperty("variant")) {
-            console.log("V");
-            var actor = msg.info.actor;
-            var id = msg.info.multipv_ind;
-            if (!(actor in VariantInfo)) {
-                VariantInfo[actor] = {}
-            }
-            if (id == 1) FenRef[actor] = msg["fenref"];
-            var hd = ""
-            if ("nps" in msg.info) {
-                hd += " | Nps: " + msg.info.nps;
-            }
-            if (id == 1 && "score" in msg.info) {
-                hd += " | Score: " + msg.info.score;
-            }
-            if ("depth" in msg.info) {
-                hd += " | Depth: " + msg.info.depth;
-                if ("seldepth" in msg.info) {
-                    hd += "/" + msg.info.seldepth;
+                if (id == 1 && "score" in msg.info) {
+                    hd += " | Score: " + msg.info.score;
                 }
-            }
-            if ("tbhits" in msg.info) {
-                hd += " | TbHits: " + msg.info["tbhits"];
-            }
-            hd += " |";
-            StatHeader[actor] = hd;
-            var htmlpgn = "<div class=\"variant\">[" + msg.info.score + "] &nbsp;";
-            for (var mvi in msg.info.variant) {
-                if (mvi == "fen") continue;
-                var mv = msg.info.variant[mvi];
-                if (mvi != 0) htmlpgn += "&nbsp;";
-                var mv1 = mv[1].replace('-', '‑'); // make dash non-breaking
-                var mv2 = "";
-                if (mv.length > 2) {
-                    var mv2 = mv[2].replace('-', '‑');
+                if ("depth" in msg.info) {
+                    hd += " | Depth: " + msg.info.depth;
+                    if ("seldepth" in msg.info) {
+                        hd += "/" + msg.info.seldepth;
+                    }
                 }
-                htmlpgn += "<span class=\"movenr\">" + mv[0] + ".</span>&nbsp;" + mv1 + "&nbsp;" + mv2 + " ";
-            }
-            htmlpgn += "</div>";
-            VariantInfo[actor][id] = htmlpgn
+                if ("tbhits" in msg.info) {
+                    hd += " | TbHits: " + msg.info["tbhits"];
+                }
+                hd += " |";
+                StatHeader[actor] = hd;
+                var htmlpgn = "<div class=\"variant\">[" + msg.info.score + "] &nbsp;";
+                for (var mvi in msg.info.variant) {
+                    if (mvi == "fen") continue;
+                    var mv = msg.info.variant[mvi];
+                    if (mvi != 0) htmlpgn += "&nbsp;";
+                    var mv1 = mv[1].replace('-', '‑'); // make dash non-breaking
+                    var mv2 = "";
+                    if (mv.length > 2) {
+                        var mv2 = mv[2].replace('-', '‑');
+                    }
+                    htmlpgn += "<span class=\"movenr\">" + mv[0] + ".</span>&nbsp;" + mv1 + "&nbsp;" + mv2 + " ";
+                }
+                htmlpgn += "</div>";
+                VariantInfo[actor][id] = htmlpgn
 
-            var n = 0;
-            for (var i in VariantInfo) {
-                var ai = VariantInfo[i];
-                var htmlpi = "";
-                for (var j in ai) {
-                    htmlpi += ai[j];
+                var n = 0;
+                for (var i in VariantInfo) {
+                    var ai = VariantInfo[i];
+                    var htmlpi = "";
+                    for (var j in ai) {
+                        htmlpi += ai[j];
+                    }
+                    if (n == 0) {
+                        document.getElementById("miniinfo1").innerHTML = htmlpi;
+                        document.getElementById("ph2").innerText = i;
+                        miniBoard1.setPosition(FenRef[i], false);
+                        document.getElementById("ph21").innerText = StatHeader[i];
+                    }
+                    if (n == 1) {
+                        document.getElementById("miniinfo2").innerHTML = htmlpi;
+                        document.getElementById("ph3").innerText = i;
+                        miniBoard2.setPosition(FenRef[i], false);
+                        document.getElementById("ph31").innerText = StatHeader[i];
+                    }
+                    n += 1;
                 }
-                if (n == 0) {
-                    document.getElementById("miniinfo1").innerHTML = htmlpi;
-                    document.getElementById("ph2").innerText = i;
-                    miniBoard1.setPosition(FenRef[i], false);
-                    document.getElementById("ph21").innerText = StatHeader[i];
-                }
-                if (n == 1) {
-                    document.getElementById("miniinfo2").innerHTML = htmlpi;
-                    document.getElementById("ph3").innerText = i;
-                    miniBoard2.setPosition(FenRef[i], false);
-                    document.getElementById("ph31").innerText = StatHeader[i];
-                }
-                n += 1;
             }
         }
     }
-
 }
