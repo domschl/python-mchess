bd29aa9ed6b442db7f82107d03af937e20e429ba
Dominik Schlösser <dsc@dosc.net>
2020-05-01 21:18:24 +0200

    v0.2.1

--- conf.py
+++ conf.py
@@ -26,7 +26,7 @@ author = 'Dominik Schlösser'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.2.1'
 
 
 # -- General configuration ---------------------------------------------------
