49ed0b0d48e56371071f0c8cae08caf17eaf64a9
Dominik Schlösser <dsc@dosc.net>
2023-09-24 18:40:14 +0200

    Incomplete

--- mchess/async_web_agent.py
+++ mchess/async_web_agent.py
@@ -1,12 +1,12 @@
 ''' Web interface using aiohttp '''
 import logging
-import time
-import queue
+# import time
+# import queue
 import json
-import os
+# import os
 import threading
 import asyncio
-import copy
+# import copy
 import aiohttp
 from aiohttp import web
 import chess
@@ -116,13 +116,18 @@ class AsyncWebAgent:
         return web.FileResponse('web/styles/mchess.css')
 
     def send2ws(self, ws, text):
+        if ws.closed:
+            self.log.warning(f"Closed websocket encountered: {ws}")
+            return False
         asyncio.run_coroutine_threadsafe(ws.send_str(text), self.event_loop)
+        return True
 
     async def websocket_handler(self, request):
-        ws = web.WebSocketResponse()
+        ws = web.WebSocketResponse(autoclose=False)
         await ws.prepare(request)
 
-        self.ws_clients.append(ws)
+        if ws not in self.ws_clients:
+            self.ws_clients.append(ws)
 
         if self.last_board is not None and self.last_attribs is not None:
             msg = {'cmd': 'display_board', 'fen': self.last_board.fen(), 'pgn': self.last_pgn,
@@ -167,6 +172,9 @@ class AsyncWebAgent:
                 self.log.warning(f'ws connection closed with exception {ws.exception()}')
             else:
                 self.log.error(f"Unexpected message {msg.data}, of type {msg.type}")
+            
+        self.log.warning(f"WS-CLOSE: {ws}")
+        self.ws_clients.remove(ws)
 
         return ws
 
@@ -193,26 +201,29 @@ class AsyncWebAgent:
                'attribs': attribs}
         for ws in self.ws_clients:
             try:
-                self.send2ws(ws, json.dumps(msg))
+                if self.send2ws(ws, json.dumps(msg)) is False:
+                    self.ws_clients.remove(ws)
             except Exception as e:
                 self.log.warning(
                     "Sending board to WebSocket client {} failed with {}".format(ws, e))
 
     def display_move(self, move_msg):
+        self.log.info(f"AWS display move to {len(self.ws_clients)} clients")
         self.display_move_cache = move_msg
         for ws in self.ws_clients:
             try:
-                self.send2ws(ws, json.dumps(move_msg))
+                if self.send2ws(ws, json.dumps(move_msg)) is False:
+                    self.ws_clients.remove(ws)
             except Exception as e:
                 self.log.warning(
-                    "Sending display_move to WebSocket client {} failed with {}".format(ws, e))
+                    f"Sending display_move {move_msg} to WebSocket client {ws} failed with {e}")
 
     def set_valid_moves(self, board, vals):
         self.log.info("web set valid called.")
         self.valid_moves_cache = {
             "cmd": "valid_moves",
             "valid_moves": [],
-            'actor': 'WebAgent'
+            'actor': 'AsyncWebAgent'
         }
         if vals is not None:
             for v in vals:
@@ -220,18 +231,20 @@ class AsyncWebAgent:
         self.log.info(f"Valid-moves: {self.valid_moves_cache}")
         for ws in self.ws_clients:
             try:
-                self.send2ws(ws, json.dumps(self.valid_moves_cache))
+                if self.send2ws(ws, json.dumps(self.valid_moves_cache)) is False:
+                    self.ws_clients.remove(ws)
             except Exception as e:
                 self.log.warning(
-                    "Sending display_move to WebSocket client {} failed with {}".format(ws, e))
+                    "Sending valid_moves to WebSocket client {} failed with {}".format(ws, e))
 
     def display_info(self, board, info):
         for ws in self.ws_clients:
             try:
-                self.send2ws(ws, json.dumps(info))
+                if self.send2ws(ws, json.dumps(info)) is False:
+                    self.ws_clients.remove(ws)
             except Exception as e:
                 self.log.warning(
-                    "Sending move-info to WebSocket client {} failed with {}".format(ws, e))
+                    "Sending display-info to WebSocket client {} failed with {}".format(ws, e))
 
     def engine_list(self, msg):
         for engine in msg["engines"]:
@@ -239,18 +252,20 @@ class AsyncWebAgent:
         self.uci_engines_cache = msg
         for ws in self.ws_clients:
             try:
-                self.send2ws(ws, json.dumps(msg))
+                if self.send2ws(ws, json.dumps(msg)) is False:
+                    self.ws_clients.remove(ws)
             except Exception as e:
                 self.log.warning(
                     "Sending uci-info to WebSocket client {} failed with {}".format(ws, e))
 
     def game_stats(self, stats):
-        msg = {'cmd': 'game_stats', 'stats': stats, 'actor': 'WebAgent'}
+        msg = {'cmd': 'game_stats', 'stats': stats, 'actor': 'AsyncWebAgent'}
         self.game_stats_cache = msg
         self.log.info(f"Game stats: {msg}")
         for ws in self.ws_clients:
             try:
-                self.send2ws(ws, json.dumps(msg))
+                if self.send2ws(ws, json.dumps(msg)) is False:
+                    self.ws_clients.remove(ws)
             except Exception as e:
                 self.log.warning(
                     "Sending game_stats to WebSocket client {} failed with {}".format(ws, e))
@@ -259,182 +274,8 @@ class AsyncWebAgent:
         self.agent_state_cache[msg['actor']] = msg
         for ws in self.ws_clients:
             try:
-                self.send2ws(ws, json.dumps(msg))
+                if self.send2ws(ws, json.dumps(msg)) is False:
+                    self.ws_clients.remove(ws)
             except Exception as e:
                 self.log.warning(
                     "Sending agent-state info to WebSocket client {} failed with {}".format(ws, e))
-
-
-
-class NotPorted:
-    def ws_dispatch(self, ws, message):
-        # self.log.info(f"message received: {message}")
-        if message is not None:
-            self.log.debug(
-                "Client ws_dispatch: ws:{} msg:{}".format(ws, message))
-            try:
-                self.log.info(f"Received: {message}")
-                self.appque.put(json.loads(message))
-            except Exception as e:
-                self.log.debug("WebClient sent invalid JSON: {}".format(e))
-
-    def ws_sockets(self, ws):
-        self.ws_handle += 1
-        handle = self.ws_handle
-        if self.last_board is not None and self.last_attribs is not None:
-            msg = {'cmd': 'display_board', 'fen': self.last_board.fen(), 'pgn': self.last_pgn,
-                   'attribs': self.last_attribs}
-            try:
-                ws.send(json.dumps(msg))
-            except Exception as e:
-                self.log.warning(
-                    "Sending to WebSocket client {} failed with {}".format(handle, e))
-                return
-        for actor in self.agent_state_cache:
-            msg = self.agent_state_cache[actor]
-            try:
-                ws.send(json.dumps(msg))
-            except Exception as e:
-                self.log.warning(
-                    f"Failed to update agents states to new web-socket client: {e}")
-        if self.uci_engines_cache != {}:
-            ws.send(json.dumps(self.uci_engines_cache))
-        if self.display_move_cache != {}:
-            ws.send(json.dumps(self.display_move_cache))
-        if self.valid_moves_cache != {}:
-            ws.send(json.dumps(self.valid_moves_cache))
-        if self.game_stats_cache != {}:
-            ws.send(json.dumps(self.game_stats_cache))
-        self.ws_clients[handle] = ws
-        while not ws.closed:
-            message = ws.receive()
-            self.ws_dispatch(handle, message)
-        del self.ws_clients[handle]
-
-#    def sock_connect(self):
-#        print("CONNECT")
-
-#    def sock_message(self, message):
-#        print("RECEIVED: {}".format(message))
-
-    def display_board(self, board, attribs={'unicode': True, 'invert': False, 'white_name': 'white', 'black_name': 'black'}):
-        self.last_board = board
-        self.last_attribs = attribs
-        try:
-            game = chess.pgn.Game().from_board(board)
-            game.headers["White"] = attribs["white_name"]
-            game.headers["Black"] = attribs["black_name"]
-            pgntxt = str(game)
-        except Exception as e:
-            self.log.error("Invalid PGN position, {}".format(e))
-            return
-        self.last_pgn = pgntxt
-        # print("pgn: {}".format(pgntxt))
-        msg = {'cmd': 'display_board', 'fen': board.fen(), 'pgn': pgntxt,
-               'attribs': attribs}
-        for w in self.ws_clients:
-            try:
-                self.ws_clients[w].send(json.dumps(msg))
-            except Exception as e:
-                self.log.warning(
-                    "Sending board to WebSocket client {} failed with {}".format(w, e))
-
-    def display_move(self, move_msg):
-        self.display_move_cache = move_msg
-        for w in self.ws_clients:
-            try:
-                self.ws_clients[w].send(json.dumps(move_msg))
-            except Exception as e:
-                self.log.warning(
-                    "Sending display_move to WebSocket client {} failed with {}".format(w, e))
-
-    def set_valid_moves(self, board, vals):
-        self.log.info("web set valid called.")
-        self.valid_moves_cache = {
-            "cmd": "valid_moves",
-            "valid_moves": [],
-            'actor': 'WebAgent'
-        }
-        if vals is not None:
-            for v in vals:
-                self.valid_moves_cache['valid_moves'].append(vals[v])
-        self.log.info(f"Valid-moves: {self.valid_moves_cache}")
-        for w in self.ws_clients:
-            try:
-                self.ws_clients[w].send(json.dumps(self.valid_moves_cache))
-            except Exception as e:
-                self.log.warning(
-                    "Sending display_move to WebSocket client {} failed with {}".format(w, e))
-
-    def display_info(self, board, info):
-        for w in self.ws_clients:
-            try:
-                self.ws_clients[w].send(json.dumps(info))
-            except Exception as e:
-                self.log.warning(
-                    "Sending move-info to WebSocket client {} failed with {}".format(w, e))
-
-    def engine_list(self, msg):
-        for engine in msg["engines"]:
-            self.log.info(f"Engine {engine} announced.")
-        self.uci_engines_cache = msg
-        for w in self.ws_clients:
-            try:
-                self.ws_clients[w].send(json.dumps(msg))
-            except Exception as e:
-                self.log.warning(
-                    "Sending uci-info to WebSocket client {} failed with {}".format(w, e))
-
-    def game_stats(self, stats):
-        msg = {'cmd': 'game_stats', 'stats': stats, 'actor': 'WebAgent'}
-        self.game_stats_cache = msg
-        self.log.info(f"Game stats: {msg}")
-        for w in self.ws_clients:
-            try:
-                self.ws_clients[w].send(json.dumps(msg))
-            except Exception as e:
-                self.log.warning(
-                    "Sending game_stats to WebSocket client {} failed with {}".format(w, e))
-
-    def agent_states(self, msg):
-        self.agent_state_cache[msg['actor']] = msg
-        for w in self.ws_clients:
-            try:
-                self.ws_clients[w].send(json.dumps(msg))
-            except Exception as e:
-                self.log.warning(
-                    "Sending agent-state info to WebSocket client {} failed with {}".format(w, e))
-
-    # def set_valid_moves(self, board, vals):
-    #     self.socket_moves = []
-    #     if vals != None:
-    #         for v in vals:
-    #             self.socket_moves.append(vals[v])
-
-    def socket_event_worker_thread(self, appque, log, app, WebSocketHandler):
-        if self.bind_address == '0.0.0.0':
-            address = socket.gethostname()
-        else:
-            address = self.bind_address
-
-        if self.private_key is None or self.public_key is None:
-            server = pywsgi.WSGIServer(
-                (self.bind_address, self.port), app, handler_class=WebSocketHandler)
-            protocol = 'http'
-            self.log.info(f"Web browser: {protocol}://{address}:{self.port}")
-        else:
-            server = pywsgi.WSGIServer(
-                (self.bind_address, self.port), app, keyfile=self.private_key, certfile=self.public_key, handler_class=WebSocketHandler)
-            protocol = 'https'
-            self.log.info(f"Web browser: {protocol}://{address}:{self.port}")
-        print(f"Web browser: {protocol}://{address}:{self.port}")
-        server.serve_forever()
-
-    def socket_handler(self):
-        self.socket_thread_active = True
-
-        self.socket_event_thread = threading.Thread(
-            target=self.socket_event_worker_thread, args=(self.appque, self.log, self.app, WebSocketHandler))
-        self.socket_event_thread.setDaemon(True)
-        self.socket_event_thread.start()
-
